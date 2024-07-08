f3785cd22768d25915042858dd75a5843941f216
Mong Israel <mongsolomon@gmail.com>
2024-07-08 14:22:20 +0100

    added the final commit

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,8 +3,81 @@ import jwt from 'jsonwebtoken';
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
 import { jwtTokens } from '../utils/jwt-helpers.js';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
+
+
+// Create a User
+router.post(
+  '/register',
+  [
+    body('firstName').not().isEmpty().withMessage('First name is required'),
+    body('lastName').not().isEmpty().withMessage('Last name is required'),
+    body('email').isEmail().withMessage('Email is invalid'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('phone').isMobilePhone().withMessage('Phone number is invalid'),
+  ],
+  async (req, res) => {
+    try {
+      // Check for validation errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ 
+          errors: 
+          // errors.array()
+          errors.array().map(val=>{
+          return {
+            field: val.path,
+            message: val.msg
+          }
+        })
+      });
+      }
+
+      const { firstName, lastName, email, password, phone } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await pool.query(
+        'INSERT INTO users (firstName, lastName, email, password, phone) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [firstName, lastName, email, hashedPassword, phone]
+      );
+
+      const userId = newUser.rows[0].userid;
+
+      const orgName = `${firstName}'s Organisation`;
+      const newOrg = await pool.query(
+        'INSERT INTO organisations (name, description) VALUES ($1, $2) RETURNING *',
+        [orgName, 'Default organisation description']
+      );
+
+      const orgId = newOrg.rows[0].orgid;
+      await pool.query(
+        'INSERT INTO user_organisations (userId, orgId) VALUES ($1, $2)',
+        [userId, orgId]
+      );
+
+      res.status(201).json(
+        // jwtTokens(newUser.rows[0])
+        {
+          status: "success",
+          message: "Registration Successful",
+          data: {
+            accessToken: jwtTokens(newUser.rows[0]).accessToken,
+            user: { userId, firstName, lastName, email, phone
+            }
+          }
+        }
+      );
+    } catch (error) {
+      res.status(400).json({ 
+        status: "Bad request",
+        message: error.message,
+        // message: "Registration Unsuccessful",
+        statusCode: 400
+      });
+    }
+  }
+);
 
 router.post('/login', async (req, res) => {
   try {
@@ -17,7 +90,7 @@ router.post('/login', async (req, res) => {
     const validPassword = await bcrypt.compare(password, users.rows[0].password);
     if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
 
-    
+
     // JWT
     let tokens = jwtTokens(users.rows[0]); // Gets access and refresh tokens
     res.cookie('refresh_token', tokens.refreshToken, { ...(process.env.COOKIE_DOMAIN && { domain: process.env.COOKIE_DOMAIN }), httpOnly: true, sameSite: 'none', secure: true });
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import pool from '../db.js';
-import bcrypt from 'bcrypt';
 import { authenticateToken } from '../middleware/authorization.js';
-import { jwtTokens } from '../utils/jwt-helpers.js';
-import { body, validationResult } from 'express-validator';
 
 
 const router = express.Router();
@@ -50,77 +47,6 @@ router.get('/:id', authenticateToken, async (req, res) => {
 });
 
 
-
-// Create a User
-router.post(
-  '/',
-  [
-    body('firstName').not().isEmpty().withMessage('First name is required'),
-    body('lastName').not().isEmpty().withMessage('Last name is required'),
-    body('email').isEmail().withMessage('Email is invalid'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    body('phone').isMobilePhone().withMessage('Phone number is invalid'),
-  ],
-  async (req, res) => {
-    try {
-      // Check for validation errors
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json({ 
-          errors: 
-          // errors.array()
-          errors.array().map(val=>{
-          return {
-            field: val.path,
-            message: val.msg
-          }
-        })
-      });
-      }
-
-      const { firstName, lastName, email, password, phone } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = await pool.query(
-        'INSERT INTO users (firstName, lastName, email, password, phone) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [firstName, lastName, email, hashedPassword, phone]
-      );
-
-      const userId = newUser.rows[0].userid;
-
-      const orgName = `${firstName}'s Organisation`;
-      const newOrg = await pool.query(
-        'INSERT INTO organisations (name, description) VALUES ($1, $2) RETURNING *',
-        [orgName, 'Default organisation description']
-      );
-
-      const orgId = newOrg.rows[0].orgid;
-      await pool.query(
-        'INSERT INTO user_organisations (userId, orgId) VALUES ($1, $2)',
-        [userId, orgId]
-      );
-
-      res.status(201).json(
-        // jwtTokens(newUser.rows[0])
-        {
-          status: "success",
-          message: "Registration Successful",
-          data: {
-            accessToken: jwtTokens(newUser.rows[0]).accessToken,
-            user: { userId, firstName, lastName, email, phone
-            }
-          }
-        }
-      );
-    } catch (error) {
-      res.status(400).json({ 
-        status: "Bad request",
-        message: error.message,
-        // message: "Registration Unsuccessful",
-        statusCode: 400
-      });
-    }
-  }
-);
 
 
 router.delete('/', async (req, res) => {
